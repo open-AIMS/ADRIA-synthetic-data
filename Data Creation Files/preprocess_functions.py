0aa68627d27ffbdbd0d1f368beb4944c2243b348
Rose Crocker <r.crocker@aims.gov.au>
2023-02-16 16:32:31 +1000

    Add function docs to preprocess functions

--- Data Creation Files/preprocess_functions.py
+++ Data Creation Files/preprocess_functions.py
@@ -3,7 +3,12 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 
 def preprocess_site_data(site_data_geo):
+    """
+    Preprocess site data into correct format for TVAE model.
 
+    :param dataframe site_data_geo: contains site data with :geometry column.
+
+    """
     site_data = site_data_geo[site_data_geo.columns[:-1]]
     site_data["long"] = site_data_geo.centroid.x
     site_data["lat"] = site_data_geo.centroid.y
@@ -20,30 +25,31 @@ def preprocess_site_data(site_data_geo):
     site_data['long'] = abs(site_data['long'])
     site_data['lat'] = abs(site_data['lat'])
     # define table metadata
-    metadata = {
-        'fields': {
-            'site_id': {'type': 'id', 'subtype': 'integer'},
-            'habitat': {'type': 'categorical'},
-            'k': {'type': 'numerical', 'subtype': 'float'},
-            'area': {'type': 'numerical', 'subtype': 'float'},
-            'rubble': {'type': 'numerical', 'subtype': 'float'},
-            'sand': {'type': 'numerical', 'subtype': 'float'},
-            'rock': {'type': 'numerical', 'subtype': 'float'},
-            'coral_algae': {'type': 'numerical', 'subtype': 'float'},
-            'na_proportion': {'type': 'numerical', 'subtype': 'float'},
-            'depth_mean': {'type': 'numerical', 'subtype': 'float'},
-            'depth_sd': {'type': 'numerical', 'subtype': 'float'},
-            'depth_med': {'type': 'numerical', 'subtype': 'float'},
-            'zone_type': {'type': 'categorical'},
-            'long': {'type': 'numerical', 'subtype': 'float'},
-            'lat': {'type': 'numerical', 'subtype': 'float'}
-        },
-        'constraints': [],
-        'primary_key': 'site_id'
-    }
+    metadata = {'fields': {'site_id': {'type': 'id'},
+                            'habitat': {'type': 'categorical'},
+                            'k': {'type': 'numerical', 'subtype': 'float'},
+                            'area': {'type': 'numerical', 'subtype': 'float'},
+                            'rubble': {'type': 'numerical', 'subtype': 'float'},
+                            'sand': {'type': 'numerical', 'subtype': 'float'},
+                            'rock': {'type': 'numerical', 'subtype': 'float'},
+                            'coral_algae': {'type': 'numerical', 'subtype': 'float'},
+                            'na_proportion': {'type': 'numerical', 'subtype': 'float'},
+                            'depth_mean': {'type': 'numerical', 'subtype': 'float'},
+                            'depth_sd': {'type': 'numerical', 'subtype': 'float'},
+                            'depth_med': {'type': 'numerical', 'subtype': 'float'},
+                            'zone_type': {'type': 'categorical'},
+                             'long': {'type': 'numerical', 'subtype': 'float'},
+                              'lat': {'type': 'numerical', 'subtype': 'float'}},
+                'primary_key':'site_id'}
     return site_data, metadata
 
 def preprocess_dhw_data(dhw_data):
+    """
+    Preprocesses dhw into correct form for PAR model.
+
+    :param numpy array dhw_data: contains original dhw data, loaded from nc file.
+
+    """
     nyears, nsites, nreps = dhw_data['dhw'].shape
     size = nyears*nsites
 
@@ -86,6 +92,14 @@ def preprocess_dhw_data(dhw_data):
 
 
 def tide_dist(latlong_site1, latlong_site2):
+    """
+    Calculates matrix of tital distances given input lat and long.
+    (used to complement connectivity data).
+
+    :param vec latlong_site1: [lat, long] for first site.
+    :param vec latlong_site2: [lat, long] for 2nd site.
+
+    """
     a2 = latlong_site1 * np.pi / 180  # convert to radians
     b2 = latlong_site2 * np.pi / 180
     a = (np.sin((b2 - a2) / 2)) ** 2
@@ -95,6 +109,13 @@ def tide_dist(latlong_site1, latlong_site2):
     return d
 
 def preprocess_conn_data(site_data,conn_orig):
+    """
+    Preprocesses connectivity data byt appending NS and ES tidal data.
+
+    :param dataframe site_data: original site data.
+    :param dataframe conn_orig: original connectivity data.
+
+    """
     lats = site_data.lats
     longs = site_data.longs
     ### ----------------Preprocessing data and appending tidal distance matrices------------------###
@@ -103,8 +124,7 @@ def preprocess_conn_data(site_data,conn_orig):
     rec_sites = conn_orig["recieving_site"]
 
     conn_data.drop(conn_data.columns[0], axis=1, inplace=True)
-    # scaler_conn = MinMaxScaler().fit(conn_data)
-    # conn_data = scaler_conn.transform(conn_data)
+
     conn_data = pd.DataFrame(conn_data, columns=conn_orig.columns)
     print(conn_data.isnull().values.any())
     conn_data.fillna(0)
@@ -134,4 +154,8 @@ def preprocess_conn_data(site_data,conn_orig):
     conn_data = pd.concat([rec_sites, conn_data], axis=1)
     conn_data.rename(columns={0: "recieving_site"}, inplace=True)
 
-    return conn_data, conn_orig, scaler
+    conn_fields = {kk : {'type':'numerical','subtype':'float'} for kk in conn_orig.columns}
+    # create metadata dictionary
+    metadata_conn = {'fields': conn_fields,
+                'primary_key':'recieving_site'}
+    return conn_data, conn_orig, scaler, metadata_conn
