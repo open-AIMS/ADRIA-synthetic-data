aef1dbcfd021a9b45934381983a4cf4f3b2d13ed
Rose Crocker <r.crocker@aims.gov.au>
2023-03-01 08:19:06 +1000

    Correct name for example synthetic site data

--- examples/coral-cover-generation.py
+++ examples/coral-cover-generation.py
@@ -10,10 +10,10 @@ from sdmetrics.reports.single_table import QualityReport
 ### --------------------- Use model to generate synthetic covers and sampled synthetic cover ------------------- ###
 
 root_original_file = 'Moore_2022-11-17'
-root_site_data_synth = 'site_data_23-2-2023_10551_numsamps_30.csv'
+root_site_data_synth = 'site_data_28-2-2023_164233_numsamps_30.csv'
 
 cover_df, synth_cover, synth_sampled, metadata_cover, root_site_data_synth = coral_cover_model(root_original_file, root_site_data_synth, 300)
-
+breakpoint()
 ### --------------- Evaluate synthetic data and sampled data utility and generate quality report --------------- ###
 
 # evaluate K-S score
