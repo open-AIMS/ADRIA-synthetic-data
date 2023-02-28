caf288e9e2048e54f2d91942ea6cab6562b4fbf1
Rose Crocker <r.crocker@aims.gov.au>
2023-02-28 16:52:40 +1000

    Use absolute paths when creating file names for loading and saving data

--- src/data_processing/package_synth_data.py
+++ src/data_processing/package_synth_data.py
@@ -1,13 +1,14 @@
 import os
 
 global SYNTH_DATA_PACKAGE_DIR, SYNTH_DATA_DIR, ORIG_DATA_DIR
-SYNTH_DATA_PACKAGE_DIR = "synthetic_data\\synthetic_data_packages\\"
-SYNTH_DATA_DIR = "synthetic_data\\"
-ORIG_DATA_DIR = "original_data\\"
+file_dir = os.path.dirname(os.path.abspath(__file__))
+SYNTH_DATA_PACKAGE_DIR = file_dir[:-19]+"synthetic_data\\synthetic_data_packages\\"
+SYNTH_DATA_DIR = file_dir[:-19]+"synthetic_data\\"
+ORIG_DATA_DIR = file_dir[:-19]+"original_data\\"
 
 
 def initialize_data_package(synth_data_stamp):
-    synth_data_set_folder = SYNTH_DATA_DIR+synth_data_stamp
+    synth_data_set_folder = SYNTH_DATA_PACKAGE_DIR+synth_data_stamp
 
     SITE_DATA_DIR = os.path.join(synth_data_set_folder, "site_data")
     CONN_DATA_DIR = os.path.join(synth_data_set_folder, "connectivity","2000")
@@ -17,8 +18,14 @@ def initialize_data_package(synth_data_stamp):
     os.makedirs(CONN_DATA_DIR)
     os.makedirs(DHW_DATA_DIR)
 
-def retrieve_synth_site_data_fp(synth_site_data_fn):
-    return SYNTH_DATA_DIR+synth_site_data_fn
+def create_synth_site_data_package_fp(time_stamp,N):
+    return SYNTH_DATA_PACKAGE_DIR+time_stamp+'_numsamps_'+str(N)+'\\site_data\\site_data_anon_'+time_stamp+'_numsamps_'+str(N)+'.csv'
+
+def create_synth_site_fp(time_stamp,N):
+    return SYNTH_DATA_DIR+'site_data_'+time_stamp+'_numsamps_'+str(N)+'.csv'
+
+def retrieve_synth_site_fp(synth_data_fn):
+    return SYNTH_DATA_DIR+synth_data_fn
 
 def retrieve_orig_site_data_fp(orig_data_package,file_type):
     return ORIG_DATA_DIR+orig_data_package+"\\site_data\\"+orig_data_package+file_type
