4b09afc4cb112daab589444ff9e473bb50cc5e2e
Rose Crocker <r.crocker@aims.gov.au>
2023-03-01 09:10:27 +1000

    Add '..' to sys path + add number of samples input to env data model

--- examples/env-data-generation.py
+++ examples/env-data-generation.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append("..")
+
 from src.plotting.data_comparison_plots import create_timeseries, get_data_quantiles
 from src.models.env_PAR_model import env_data_model
 
@@ -12,10 +15,11 @@ from sdmetrics.reports.single_table import QualityReport
 layer = 'Ub'
 rcp = '45'
 root_original_file = 'Moore_2022-11-17'
-root_site_data_synth = 'site_data_23-2-2023_10551_numsamps_30.csv'
+root_site_data_synth = 'site_data_28-2-2023_164233_numsamps_30.csv'
+nsamples = 30
 
-env_df, new_data_env, selected_env_data, metadata_env, nyears, old_years = env_data_model(root_original_file, root_site_data_synth, rcp, layer)
-
+env_df, new_data_env, selected_env_data, metadata_env, nyears, old_years = env_data_model(root_original_file, root_site_data_synth, nsamples, rcp, layer)
+breakpoint()
 ### --------------- Evaluate synthetic data and sampled data utility and generate quality report --------------- ###
 
 # detection values should be low
