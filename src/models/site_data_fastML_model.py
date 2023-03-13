32e8980d99f9a429bc8cb46ebd50fe09ed58ed1f
Rose Crocker <r.crocker@aims.gov.au>
2023-03-14 08:53:45 +1000

    Change function used to anonymize site data in site data model

--- src/models/site_data_fastML_model.py
+++ src/models/site_data_fastML_model.py
@@ -4,7 +4,7 @@ from sdv.lite import TabularPreset
 
 from src.data_processing.preprocess_functions import preprocess_site_data
 from src.data_processing.sampling_functions import sample_rand_radii
-from src.data_processing.postprocess_functions import anonymize_spatial, generate_timestamp
+from src.data_processing.postprocess_functions import anonymize_spatial, generate_timestamp,convert_to_geo
 from src.data_processing.package_synth_data import initialize_data_package,retrieve_orig_site_data_fp,create_synth_site_data_fp,create_synth_site_data_package_fp
 
 def site_data_model(orig_data_package, N, N2, N3):
@@ -33,13 +33,14 @@ def site_data_model(orig_data_package, N, N2, N3):
     ### ------------------------------------------Save site data to csv-------------------------------------------###
     time_stamp = generate_timestamp()
     sample_sites['lat'] = -1*sample_sites['lat']
-    breakpoint()
-    sample_sites_fn = create_synth_site_data_fp(time_stamp,N2)
-    sample_sites.to_csv(sample_sites_fn)
-    sample_sites_anon = anonymize_spatial(sample_sites)
+    sample_sites['reef_siteid'] = ['reef_'+str(k) for k in range(1,len(sample_sites['siteid']))]
+    sample_sites_fn = create_synth_site_data_fp(time_stamp)
+    sample_sites.to_csv(sample_sites_fn, index = False)
+    sample_sites_geo = convert_to_geo(sample_sites)
+    sample_sites_anon = anonymize_spatial(sample_sites_geo)
 
-    initialize_data_package(time_stamp+'_numsamps_'+str(N2))
-    synth_site_data_fn = create_synth_site_data_package_fp(time_stamp,N2)
-    sample_sites_anon.to_csv(synth_site_data_fn)
+    initialize_data_package(time_stamp)
+    synth_site_data_fn = create_synth_site_data_package_fp('synth_'+time_stamp)
+    sample_sites_anon.to_file(synth_site_data_fn)
 
     return site_data, new_site_data, sample_sites, metadata_site, synth_site_data_fn
