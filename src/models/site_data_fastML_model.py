9b1bcbca11f0b2e560156af6f3bb76db97cb07da
Rose Crocker <r.crocker@aims.gov.au>
2023-03-28 13:33:45 +1000

    Correct how new site ids defined in site data model

--- src/models/site_data_fastML_model.py
+++ src/models/site_data_fastML_model.py
@@ -11,7 +11,7 @@ def site_data_model(orig_data_package, N, N2, N3):
     ### --------------------------------------Load site data to synethesize--------------------------------------###
 
     site_data_fn = retrieve_orig_site_data_fp(orig_data_package,".csv")
-    site_data = pd.read_csv(site_data_fn)
+    site_data = pd.read_csv(site_data_fn, index_col=False)
 
     ### ---------------------------------Preprocess data for sdv.TVAE model fit----------------------------------###
     # simplify to dataframe
@@ -33,14 +33,16 @@ def site_data_model(orig_data_package, N, N2, N3):
     ### ------------------------------------------Save site data to csv-------------------------------------------###
     time_stamp = generate_timestamp()
     sample_sites['lat'] = -1*sample_sites['lat']
-    sample_sites['reef_siteid'] = ['reef_'+str(k) for k in range(1,len(sample_sites['siteid']))]
+    
+    sample_sites['reef_siteid'] = ['reef_'+str(k) for k in range(1,len(sample_sites['site_id'])+1)]
     sample_sites_fn = create_synth_site_data_fp(time_stamp)
     sample_sites.to_csv(sample_sites_fn, index = False)
     sample_sites_geo = convert_to_geo(sample_sites)
     sample_sites_anon = anonymize_spatial(sample_sites_geo)
 
     initialize_data_package(time_stamp)
-    synth_site_data_fn = create_synth_site_data_package_fp('synth_'+time_stamp)
+
+    synth_site_data_fn = create_synth_site_data_package_fp(time_stamp)
     sample_sites_anon.to_file(synth_site_data_fn)
 
     return site_data, new_site_data, sample_sites, metadata_site, synth_site_data_fn
