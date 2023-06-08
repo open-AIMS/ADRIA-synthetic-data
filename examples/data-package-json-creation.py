5f21dc00128e570527848f9148f7ff6aa5364f0d
Rose Crocker <r.crocker@aims.gov.au>
2023-06-08 16:44:26 +1000

    Formatting

--- examples/data-package-json-creation.py
+++ examples/data-package-json-creation.py
@@ -1,9 +1,13 @@
 import sys
+
 sys.path.append("..")
 
-from src.data_processing.package_synth_data import create_dp_jason, retrieve_orig_data_package_path
+from src.data_processing.package_synth_data import (
+    create_dp_jason,
+    retrieve_orig_data_package_path,
+)
 
 orig_data_package = "Moore_2022-11-17"
-synth_data_package_name = "7-3-2023_154918_numsamps_100"
+synth_data_package_name = "synth_6-6-2023_144117"
 orig_data_package_path = retrieve_orig_data_package_path(orig_data_package)
-create_dp_jason(orig_data_package_path,synth_data_package_name)
+create_dp_jason(orig_data_package_path, synth_data_package_name)
