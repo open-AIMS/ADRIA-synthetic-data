e12132991acbb6b91ab5f16800e6f9eba4629644
Rose Crocker <r.crocker@aims.gov.au>
2023-07-27 09:43:30 +1000

    Updates to allow use of GAN model or Gaussian Copula model for connectivity

--- examples/connectivity-generation.py
+++ examples/connectivity-generation.py
@@ -5,33 +5,35 @@ sys.path.append("..")
 from src.models.connectivity_GAN_model import connectivity_model
 from src.data_processing.package_synth_data import save_csv_plotting
 
-from sdmetrics.reports.single_table import QualityReport
+from sdv.evaluation.single_table import evaluate_quality
+from sdv.metadata import SingleTableMetadata
 
 ### ------------------------------------------------Key Inputs---------------------------------------------------###
 root_original_file = "Moore_2022-11-17"
-root_site_data_synth = "synth_16-6-2023_91140.csv"
+root_site_data_synth = "synth_2023-7-24_152038.csv"
 years = ["2015", "2016", "2017"]  # connectivity data years to use
 num = ["1", "2", "3"]  # connectivity data sample number to use
-
+model_type = "GAN"  # "GaussianCopula"
 (
     conn_orig,
     conn_samples,
     selected_conn_data,
     metadata_conn,
     synth_conn_fn,
-) = connectivity_model(root_original_file, root_site_data_synth, years, num)
+) = connectivity_model(root_original_file, root_site_data_synth, years, num, model_type)
 breakpoint()
 
 ### ------------------------------------Test synthetic data utility-------------------------------------------- ###
-report = QualityReport()
-report.generate(
-    conn_orig[conn_orig.columns[0:216]],
-    conn_samples[conn_orig.columns[0:216]],
-    metadata_conn,
+# Beware - there will be a lot of warning messages for the correlation statistic because the connnectivity matrix has so many zeros
+metadata_conn = SingleTableMetadata()
+metadata_conn.detect_from_dataframe(data=conn_orig[conn_orig.columns[0:216]])
+quality_report = evaluate_quality(
+    real_data=conn_orig[conn_orig.columns[0:216]],
+    synthetic_data=conn_samples[conn_orig.columns[0:216]],
+    metadata=metadata_conn,
 )
-report.get_details(property_name="Column Shapes")
-report.get_details(property_name="Column Pair Trends")
 
+# Save original, sampled and synthetic as csvs if plotting later (only sampled is saved in data package)
 save_csv_plotting(
     conn_orig[conn_orig.columns[0:216]],
     conn_samples[conn_orig.columns[0:216]],
